c1700927be5d94ddfdf2ad78c05b4ae9c5f6d3b1
Kronk <kylec0217@gmail.com>
2022-01-25 09:21:55 -0500

    adding a new project

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import filler from './filler.png'
 import therapistFinder from './therapistFinder.png'
 import webDevGroupFind from './webDevGroupFinder.png'
 import passGenPic from'./password_generator_pic.png'
+import bubbleProject from './bubbleProject.png'
 import Header from './components/header'
 import Body from './components/body'
 import Footer from './components/footer'
 import Box from '@mui/material/Box';
 
-let picArray =[ filler, tech, therapistFinder,webDevGroupFind,passGenPic, filler]
+let picArray =[ filler, tech, therapistFinder,webDevGroupFind,passGenPic, bubbleProject, filler]
 function App(props) {
   return (
     <Box className="App" sx= {{backgroundColor:"#73c7ff", display:"flex", flexDirection:"column"}}>
